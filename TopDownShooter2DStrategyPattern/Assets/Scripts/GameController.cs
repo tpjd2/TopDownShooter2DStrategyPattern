06ea96956eb19f6efe23e521540d18b526a73212
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-11-07 15:55:16 -0300

    commit inicial

---
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instancia = null;
+    [SerializeField] private Transform player;
+
+    private void Awake()
+    {
+        if (instancia == null)
+            instancia = this;
+        else if (instancia != this)
+            Destroy(this);
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        return player.position;
+    }
+
+}
---
+++ TopDownShooter2DStrategyPattern/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    float moviment;
+    Rigidbody2D rb2d;
+    Vector3 mousePosition;
+    Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotation =
+            Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        moviment = Input.GetAxis("Vertical");
+        rb2d.AddForce(gameObject.transform.up * moviment * speed);
+    }
+}
